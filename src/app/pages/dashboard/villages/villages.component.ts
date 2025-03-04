f4fc36f7b6b3b534245dcb363798bab9c0d1a04a
Samuel Mothwa <samuelmthwa79@gmail.com>
2025-03-04 20:45:55 +0200

    updating admin portal work

---
+++ src/app/pages/dashboard/villages/villages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-villages',
+  imports: [],
+  templateUrl: './villages.component.html',
+  styleUrl: './villages.component.scss'
+})
+export class VillagesComponent {
+
+}
